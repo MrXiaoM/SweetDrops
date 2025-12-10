9ac88600dfb4a001791620fd151dab8388cdb4f1
MrXiaoM <coolxiaom95@gmail.com>
2025-12-11 01:36:32 +0800

    support 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     base.library("net.kyori:adventure-platform-bukkit:4.4.0")
     base.library("net.kyori:adventure-text-minimessage:4.23.0")
     base.library("commons-lang:commons-lang:2.6")
-    implementation("de.tr7zw:item-nbt-api:2.15.3")
+    implementation("de.tr7zw:item-nbt-api:2.15.5")
     implementation("com.github.technicallycoded:FoliaLib:0.4.4")
     for (artifact in pluginBaseModules) {
         implementation("top.mrxiaom.pluginbase:$artifact:$pluginBaseVersion")
